fc2ebf836ef0f9e7af01e26489b71648ec71367e
Kesar Karale <kesarkarale1@gmail.com>
2026-05-31 22:16:25 +0530

    Update page.tsx

--- app/dashboard/settings/page.tsx
+++ app/dashboard/settings/page.tsx
@@ -1,13 +1,19 @@
+import Sidebar from "@/components/Sidebar";
+
 export default function SettingsPage() {
   return (
-    <main className="min-h-screen bg-black text-white p-8">
-      <h1 className="text-4xl font-bold mb-6">
-        Settings
-      </h1>
+    <main className="min-h-screen bg-black text-white flex">
+      <Sidebar />
 
-      <div className="bg-zinc-900 p-6 rounded-3xl">
-        User settings will appear here.
-      </div>
+      <section className="flex-1 p-8">
+        <h1 className="text-4xl font-bold mb-6">
+          Settings
+        </h1>
+
+        <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6">
+          User settings will appear here.
+        </div>
+      </section>
     </main>
   );
 }
