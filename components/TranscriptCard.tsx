23d56a5d3b1f889fc81c278dbecb0becf12db2a2
Kesar Karale <kesarkarale1@gmail.com>
2026-05-31 21:58:12 +0530

    Update TranscriptCard.tsx

--- components/TranscriptCard.tsx
+++ components/TranscriptCard.tsx
@@ -1,4 +1,8 @@
-export default function TranscriptCard() {
+export default function TranscriptCard({
+  transcript,
+}: {
+  transcript: string;
+}) {
   return (
     <div className="bg-zinc-900 border border-zinc-800 rounded-3xl p-6 h-[420px]">
       <h2 className="text-2xl font-bold mb-4">
@@ -6,7 +10,7 @@ export default function TranscriptCard() {
       </h2>
 
       <div className="overflow-y-auto h-[320px] text-zinc-400">
-        Upload a file to generate transcript...
+        {transcript || "Upload a file to generate transcript..."}
       </div>
     </div>
   );
