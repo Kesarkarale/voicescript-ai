9814a4b4e3f372fd27113a9496aa1af427994690
Kesar Karale <kesarkarale1@gmail.com>
2026-05-31 22:10:13 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -11,25 +11,24 @@ export default function Navbar() {
         <div className="hidden md:flex gap-8 text-sm">
           <a href="#features">Features</a>
           <a href="#pricing">Pricing</a>
-          <a href="/dashboard">Dashboard</a>
+          <Link href="/dashboard">Dashboard</Link>
         </div>
 
-        <button className="bg-blue-600 px-4 py-2 rounded-lg">
-          Get Started
-        </button>
-        <Link
-   href="/sign-in"
-  className="border border-zinc-700 px-4 py-2 rounded-lg"
->
-  Login
-</Link>
+        <div className="flex items-center gap-3">
+          <Link
+            href="/sign-in"
+            className="border border-zinc-700 px-4 py-2 rounded-lg"
+          >
+            Login
+          </Link>
 
-<Link
-  href="/sign-up"
-  className="bg-blue-600 px-4 py-2 rounded-lg"
->
-  Register
-</Link>
+          <Link
+            href="/sign-up"
+            className="bg-blue-600 px-4 py-2 rounded-lg"
+          >
+            Register
+          </Link>
+        </div>
       </div>
     </nav>
   );
