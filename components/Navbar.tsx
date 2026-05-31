1926533f9f60e70ace9219dfb728e3b77f9fb577
Kesar Karale <kesarkarale1@gmail.com>
2026-05-31 22:30:59 +0530

    Update Navbar.tsx

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,11 +1,17 @@
 import Link from "next/link";
+import {
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Navbar() {
   return (
     <nav className="border-b border-zinc-800">
       <div className="mx-auto max-w-7xl px-6 py-5 flex items-center justify-between">
         <h1 className="text-2xl font-bold">
-          VoiceScript<span className="text-blue-500">AI</span>
+          VoiceScript
+          <span className="text-blue-500">AI</span>
         </h1>
 
         <div className="hidden md:flex gap-8 text-sm">
@@ -14,21 +20,36 @@ export default function Navbar() {
           <Link href="/dashboard">Dashboard</Link>
         </div>
 
-        <div className="flex items-center gap-3">
-          <Link
-            href="/sign-in"
-            className="border border-zinc-700 px-4 py-2 rounded-lg"
-          >
-            Login
-          </Link>
+        <SignedOut>
+          <div className="flex gap-3">
+            <Link
+              href="/sign-in"
+              className="border border-zinc-700 px-4 py-2 rounded-lg"
+            >
+              Login
+            </Link>
 
-          <Link
-            href="/sign-up"
-            className="bg-blue-600 px-4 py-2 rounded-lg"
-          >
-            Register
-          </Link>
-        </div>
+            <Link
+              href="/sign-up"
+              className="bg-blue-600 px-4 py-2 rounded-lg"
+            >
+              Register
+            </Link>
+          </div>
+        </SignedOut>
+
+        <SignedIn>
+          <div className="flex items-center gap-4">
+            <Link
+              href="/dashboard"
+              className="bg-blue-600 px-4 py-2 rounded-lg"
+            >
+              Dashboard
+            </Link>
+
+            <UserButton />
+          </div>
+        </SignedIn>
       </div>
     </nav>
   );
